3bf51967f8c5eafd0618b71039caf3f28fd871c8
Nicolás San Martín <igtoluh@gmail.com>
2022-11-22 16:24:14 -0300

    TG-14: updated tests and folder structure

---
+++ test/specs/locked_out_user.e2e.js
@@ -0,0 +1,46 @@
+import loginPage from '../pageobjects/login.page';
+import LoginPage from  '../pageobjects/login.page';
+
+
+describe('My Login application', () => {
+    beforeAll('Navigate to url', ()=> {
+        browser.url("https://www.saucedemo.com/");
+    })
+
+    it('Should not login with empty inputs', async () => {
+        await LoginPage.inputUsername.setValue("");
+        await LoginPage.inputPassword.setValue("");
+        await LoginPage.btnLogin.click();
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Username is required")
+    });
+    it('Should not login with empty password', async () => {
+        await LoginPage.inputUsername.setValue("locked_out_user");
+        await LoginPage.inputPassword.setValue("");
+        await LoginPage.btnLogin.click();
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Password is required");
+        await browser.refresh();
+    });
+
+    it('Should not login with empty Username', async () => {
+        await LoginPage.inputUsername.setValue("");
+        await LoginPage.inputPassword.setValue("secret_sauce");
+        await LoginPage.btnLogin.click();
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Username is required");
+    });
+
+    it('Should not login with invalid creentials', async () => {
+        await LoginPage.login('locked_out_user', 'test');
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Username and password do not match any user in this service");
+    });
+
+    it('Should login with valid credentials', async () => {
+        await LoginPage.login('locked_out_user', 'secret_sauce');
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Sorry, this user has been locked out.");
+        await browser.refresh();
+    });
+});
---
+++ test/specs/performance_glitch_user.e2e.js
@@ -0,0 +1,49 @@
+import loginPage from '../pageobjects/login.page';
+import LoginPage from  '../pageobjects/login.page';
+
+
+describe('My Login application', () => {
+    beforeAll('Navigate to url', ()=> {
+        browser.url("https://www.saucedemo.com/");
+    })
+
+    it('Should not login with empty inputs', async () => {
+        await LoginPage.inputUsername.setValue("");
+        await LoginPage.inputPassword.setValue("");
+        await LoginPage.btnLogin.click();
+
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Username is required")
+    });
+
+    it('Should not login with empty password', async () => {
+        await LoginPage.inputUsername.setValue("performance_glitch_user");
+        await LoginPage.inputPassword.setValue("");
+        await LoginPage.btnLogin.click();
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Password is required");
+        await browser.refresh();
+    });
+
+    it('Should not login with empty Username', async () => {
+        await LoginPage.inputUsername.setValue("");
+        await LoginPage.inputPassword.setValue("secret_sauce");
+        await LoginPage.btnLogin.click();
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Username is required");
+        await browser.refresh();
+    });
+
+    it('Should not login with invalid creentials', async () => {
+        await LoginPage.login('performance_glitch_user', 'test');
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText("Epic sadface: Username and password do not match any user in this service")
+    });
+    it('Should login with valid credentials', async () => {
+        await LoginPage.login('performance_glitch_user', 'secret_sauce');
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+        await LoginPage.btnBurgerMenu.click();
+        await LoginPage.btnLogout.click();
+        await browser.refresh();
+    });
+});
